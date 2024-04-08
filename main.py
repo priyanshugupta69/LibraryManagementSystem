a304b473e30ac424f6655446486313940f5e8e2b
priyanshu <priyanshuraja456@gmail.com>
2024-04-09 01:30:17 +0530

    fixed base url

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from config import db
 from config.db import movies_collection
 from routes.student import studentRouter
 db.connectToDb() 
-app.include_router(studentRouter)
+app.include_router(studentRouter, prefix="/students")
 
 
         
